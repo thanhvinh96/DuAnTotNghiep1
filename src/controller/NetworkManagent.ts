80a5a420af777d7fd3aaa8cc841b1d9d4c4ea141
phangiathuyen <thuyendi2004@gmail.com>
2024-12-02 03:11:27 +1000

    them

--- src/controller/NetworkManagent.ts
+++ src/controller/NetworkManagent.ts
@@ -45,3 +45,26 @@ export const addAdminChangeRequest = async (formData: any) => {
       throw error;
     }
   };
+
+  export const ShowAdminChangeRequest = async (formData: any) => {
+    try {
+      const response = await fetch(`http://103.179.185.78:3002/managentnetwork/changeadmin/byorg`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData), // Gửi dữ liệu đăng nhập qua body
+      });
+  
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }
+  
+      const result = await response.json();
+    
+      return result;
+    } catch (error) {
+      console.error("Error during create:", error);
+      throw error;
+    }
+  };
